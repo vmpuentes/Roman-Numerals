7d9f5ddba667bb397440babde2f904e95d076a1c
Michael Woldemedihin and Victor Puentes <victor@>
2017-07-13 14:41:33 -0700

    add specifications that can add roman numerals X and I.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,57 @@
 //businessl logic
-// var convertToRoman = function(input){
-//   var romanNumeral = {];
-//   romanNumeral = ['I','V','X','L','C','D','M'];
-//   var decimal = [];
-//   decimal = [1,5,10,50,100,500,1000];
-//   function decimalToRoman(#decimal) {
-//     for (index=0, index > 4000 )
-//   }
-// }
 var convertToRoman = function(input){
+  debugger;
   var romanNumerals = ["M", "D", "C", "L", "X", "V", "I"];
   var decimals = [1000, 500, 100, 50, 10, 5, 1];
   var romNum = "";
-  if (input === 0 || input > 3999) {
+   var counter = 1;
+  var consecutiveNumerals = [];
+  if (input <= 0 || input > 3999) {
     alert("that is not a roman numeral");
   }else {
     for (var i = 0; i < romanNumerals.length; i++) {
       while (input >= decimals[i]) {
         input -= decimals[i];
-        romNum += romanNumerals[i];
+        //romNum += romanNumerals[i];
+        consecutiveNumerals. push(romanNumerals[i]);//to track the number of consequative roman numerals
       }
     }
+    console.log(consecutiveNumerals);
+    for (var i = 0; i < consecutiveNumerals.length; i++) {
+       //var temp = consecutiveNumerals[i];
+       console.log(consecutiveNumerals[i]);
+       console.log(consecutiveNumerals[i+1]);
+         if (consecutiveNumerals[i] === consecutiveNumerals[i+1]) {
+           console.log("OH HELLO");
+           counter++;
+           if (counter === 4) {
+             var numIndex = romanNumerals.indexOf(consecutiveNumerals[i]);
+             romNum = "";
+             romNum = romanNumerals[numIndex] + romanNumerals[numIndex-2];
+             counter = 1;
+           }
+         }
+
+
+      // var counter = 0;
+      // if (counter < 4) {
+      //   temp +=consecutiveNumerals[i];
+      //   counter++;
+      // }
+    }
+    // romNum += consecutiveNumerals[i];
+    romNum = consecutiveNumerals.join("");
+    console.log(romNum);
     return romNum;
   }
 
 }
 
 
+
 $(document).ready(function(){
   $("#decimal-to-roman").submit(function(event){
         event.preventDefault();
-        //debugger;
         var decimal = $("#decimal").val();
         var output = convertToRoman(decimal);
         //alert(output);
